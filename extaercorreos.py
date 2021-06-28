5856e851ce6e026fcdc428c3db4c017c8478e6df
daniela <daniela.moreno1@mail.udp.cl>
2021-06-28 14:35:00 -0400

    Initial commit

---
+++ extaercorreos.py
@@ -0,0 +1,75 @@
+import imaplib
+import re
+from tabulate import tabulate
+from regex import getRegexId
+
+host = 'imap.gmail.com'
+imap = imaplib.IMAP4_SSL(host)
+imap.login('mail@gmail.com', 'pass')
+imap.select('Inbox')
+mails = ["info@buscalibre.com","promotion@aliexpress.com","contacto@email.easy.cl","antonia.e@integramedica.cl","noreply@cl.needishmail.com"]
+
+arrayDataMails = []
+arrayRegex= []
+for c in range(len(mails)):
+    typ, data = imap.search(None,'FROM', mails[c])
+    arrayData= data[0].split()
+    idArray = []
+    firstArray = []
+    penArray = []
+    utcArray = []
+    fromArray = []
+    mailArray = []
+    for i in range(41):#42
+        num = arrayData[i]
+        typ, data = imap.fetch(num, '(BODY[HEADER.FIELDS (MESSAGE-ID)])')
+        messageId= data[0][1].decode()
+        messageId = messageId.split("<")[1].replace(">","").strip()
+        typ, data = imap.fetch(num, '(BODY[HEADER.FIELDS (Date)])')#+1000 utc
+        messageUTC= data[0][1].decode()   
+        typ, data = imap.fetch(num, '(BODY[HEADER.FIELDS (From)])')
+        messageFrom= data[0][1].decode().replace("From:","").strip()
+        messageMail= messageFrom.split("<")[1][:-1].strip()
+        typ, data = imap.fetch(num, '(BODY[HEADER.FIELDS (Received)])')
+        messageReceived= data[0][1].decode().split("Received: ")
+        messageFirst = messageReceived[len(messageReceived)-1].strip()
+        messagePen = messageReceived[1].strip()
+        if('+' in messageFirst):
+            messageUTC = "+"+messageFirst.split("+")[1].strip()        
+        else:
+            numUTC = len(messageFirst.split("-"))
+            messageUTC = "-"+messageFirst.split("-")[numUTC-1].strip() 
+        messageUTC = messageUTC[0:5] + " (UTC)"
+
+        f = open ("res.txt", "a")
+        f.write("------------------Correo numero"+str(i)+"--------------------------\n")
+        f.write("MessageId: "+messageId+"\n")
+        f.write("Primer received: "+messageFirst.strip()+"\n")
+        f.write("Penultimo received: "+messagePen.strip()+"\n")
+        f.write("UTC: "+messageUTC+"\n")
+        f.write("From: "+messageFrom+"\n")
+        f.write("Mail: "+messageMail+"\n")
+        f.close()
+
+        idArray.append(messageId)
+        firstArray.append(messageFirst)
+        penArray.append(messagePen)
+        utcArray.append(messageUTC)
+        fromArray.append(messageFrom)
+        mailArray.append(messageMail)
+    arrayRegex.append([getRegexId(idArray),getRegexId(firstArray),getRegexId(penArray),getRegexId(utcArray),getRegexId(fromArray),getRegexId(mailArray)])
+imap.close()
+
+for i in range(len(arrayRegex)):
+    f = open ("resRegex.txt", "a")
+    f.write("---------------Correo numero "+str(i)+"----------------------\n")
+    f.write("MessageId: " + arrayRegex[i][0]+"\n")
+    f.write("Primer received: "+ arrayRegex[i][1]+"\n")
+    f.write("Penultimo received: " + arrayRegex[i][2]+"\n")
+    f.write("UTC: " + arrayRegex[i][3]+"\n")
+    f.write("From: " + arrayRegex[i][4]+"\n")
+    f.write("Mail:" +arrayRegex[i][5]+"\n")
+    f.close()
+
+
+
---
+++ regex.py
@@ -0,0 +1,126 @@
+import re
+
+#código basado
+
+def generate_regex(samples):
+    characters_list = []
+    for sample in samples:
+        characters_list = extend_characters_list(characters_list, len(sample))
+        for i, character in enumerate(sample):
+            characters_list[i].add(character)
+    return "".join(display_group(characters) for characters in characters_list)
+
+def display_group(characters):
+    if len(characters) == 1:
+        template = "{}"
+    else:
+        template = "[{}]"
+    return template.format("".join(sorted(characters)))
+
+
+def extend_characters_list(characters_list, length):
+    extension_length = max(length - len(characters_list), 0)
+    return characters_list + [set() for i in range(extension_length)]
+
+#Código propio para reducir el regex generado
+
+def dif(str1,str2):
+    minS = str1
+    maxS = str2
+    if(len(str1) > len(str2)):
+        minS=str2
+        maxS=str1
+    num = len(minS)
+    for i in range(num):
+        maxS=maxS.replace(minS[i],"", 1)
+    return(maxS)
+
+def reduceRegex(resultArray):
+    arraySintax= []
+    for i in range(len(resultArray)):
+        sintax=""
+        if(resultArray[i][0] == "["):
+            if(len(resultArray[i][1:]) > 5 ):
+                stringAux = resultArray[i][1:]
+                if(stringAux.isdigit()):
+                    sintax= "\d"
+                if(stringAux.isupper() and sintax==""):
+                    sintax= "A-Z"
+                if(stringAux.islower() and sintax==""):
+                    sintax= "a-z"
+                if(bool(re.search(r'[^A-Za-z0-9]', stringAux)) and sintax==""):
+                    specialC =""
+                    specialCArray=re.findall(r'[^A-Za-z0-9]', stringAux)
+                    for c in specialCArray:
+                        specialC= specialC +c
+                    sintax = "a-zA-Z0-9"+specialC
+                elif(sintax==""):
+                    sintax= "^A-Za-z0-9"
+                sintax = "["+sintax+"]"
+            else:
+                sintax = resultArray[i]+"]"
+        else:
+            arrayAux= resultArray[i].split("[")
+            if( len(arrayAux) > 1):
+                sintax=arrayAux[0]+"["+arrayAux[1]+"]"
+            else:
+                sintax=arrayAux[0]
+        arraySintax.append(sintax)
+
+    resFinalStr=""
+    a=0
+    while(a < len(arraySintax)):
+        numAgrup = 0
+        if(arraySintax[a][0]=="["):
+            agrupFinal =""
+            for b in range(a,len(arraySintax)):
+                resDif=dif(arraySintax[a],arraySintax[b])
+                cond = 0
+                if(len(resDif) < 4):
+                    if(len(arraySintax[a]) == len(arraySintax[b])):
+                        if(len(resDif)!= 0):
+                            arraySintax[a] = arraySintax[a][:-1] + resDif + "]"
+                            cond = 1
+                        numAgrup =numAgrup + 1                
+                    if((len(arraySintax[a])> len(arraySintax[b])) and cond == 0):
+                        arraySintax[a] = arraySintax[b][:-1]+resDif+"]"
+                        numAgrup = numAgrup +1
+                    if((len(arraySintax[a])< len(arraySintax[b])) and cond == 0):
+                        arraySintax[a] = arraySintax[a][:-1]+resDif+"]"
+                        numAgrup = numAgrup +1
+                else:
+                    agrupFinal = r"{"+ str(numAgrup) +r"}"
+                    if(numAgrup == 1):
+                        agrupFinal=""
+                    arraySintax[a]=arraySintax[a]+agrupFinal
+                    break
+        resFinalStr = resFinalStr + arraySintax[a]
+        if(numAgrup >0):
+            a=a+numAgrup
+        else:
+            a=a+1
+
+    return(resFinalStr)
+
+
+def getRegexIdFile(namefile): 
+    f = open (namefile,'r')
+    msg = f.read()
+    msgArray = msg.split()
+    f.close()
+    result = generate_regex(msgArray)
+    result2 = reduceRegex(result.split("]"))
+    return(result2)
+
+def getRegexId(msgArray): 
+    result = generate_regex(msgArray)
+    result2 = reduceRegex(result.split("]"))
+    return(result2)
+
+#A partir de un archivo:
+#a=getRegexIdFile("entrenamiento.txt")
+#print(a)
+
+#A partir de un arreglo
+#a=getRegexId(["holaaa423423423","hola4343424332"])
+#print(a)
